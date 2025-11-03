4c961d5dbd3a545efa21975bcf5df6919e44d443
FERNANDO_JOSE_ZAPA_PORTILLO <fzapaportillo@gmail.com>
2025-11-03 18:43:21 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ cv-react/src/components/CabeceraCV.jsx
@@ -0,0 +1,10 @@
+function CabeceraCV() {
+  return ( 
+  <header>
+    <h1>Fernando Jose Zapa Portillo</h1>
+    <h2>Desarrollador de software</h2>
+  </header>
+  );
+}
+
+export default CabeceraCV;
