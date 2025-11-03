67fca047964de708361a4e3124522c823faf1071
FERNANDO_JOSE_ZAPA_PORTILLO <fzapaportillo@gmail.com>
2025-11-03 18:55:55 -0500

    feat : componente perfil con descripcion personalizada

---
+++ cv-react/src/components/Perfil.jsx
@@ -0,0 +1,14 @@
+function Perfil() {
+    return (
+      <section>
+        <h3>Perfil Profesional</h3>
+        <p>
+          Soy un estudiante de desarrollo de software apasionado por la tecnología,
+          la programación y la creación de soluciones digitales.
+        </p>
+      </section>
+    );
+  }
+  
+  export default Perfil;
+  
