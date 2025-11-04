c0bf6b68402a2256d4e6f67028b0d554785c3bed
FERNANDO_JOSE_ZAPA_PORTILLO <fzapaportillo@gmail.com>
2025-11-04 06:24:56 -0500

    feat: componente Educacion con formacion complementaria

---
+++ cv-react/src/components/Educacion.jsx
@@ -0,0 +1,13 @@
+function Educacion() {
+    return (
+      <section>
+        <h3>Educación</h3>
+        <ul>
+          <li>Tecnólogo en Análisis y Desarrollo de Software - SENA</li>
+        </ul>
+      </section>
+    );
+  }
+  
+  export default Educacion;
+  
