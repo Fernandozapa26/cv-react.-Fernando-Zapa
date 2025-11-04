cc6d8c967afc4a2d5bc3e4eb56cbee467ad4d78b
FERNANDO_JOSE_ZAPA_PORTILLO <fzapaportillo@gmail.com>
2025-11-04 06:16:26 -0500

    feact: componente Experiencia con 4 cargos definidos

---
+++ cv-react/src/components/Experiencia.jsx
@@ -0,0 +1,14 @@
+function Experiencia() {
+    return (
+      <section>
+        <h3>Experiencia Laboral</h3>
+        <ul>
+          <li>Ayudante de construcción - Experiencia en obras civiles</li>
+          <li>Proyectos personales en React, Node.js y Python</li>
+        </ul>
+      </section>
+    );
+  }
+  
+  export default Experiencia;
+  
