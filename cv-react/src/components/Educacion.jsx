8adcd982d41b2de69769bc9658021b989a64c5bd
FERNANDO_JOSE_ZAPA_PORTILLO <fzapaportillo@gmail.com>
2025-11-10 11:50:57 -0500

    feat: componente Educacion dinamico con formación complementaria

--- cv-react/src/components/Educacion.jsx
+++ cv-react/src/components/Educacion.jsx
@@ -1,13 +1,49 @@
 function Educacion() {
-    return (
-      <section>
-        <h3>Educación</h3>
-        <ul>
-          <li>Tecnólogo en Análisis y Desarrollo de Software - SENA</li>
-        </ul>
-      </section>
-    );
-  }
-  
-  export default Educacion;
+    const educacion = [
+      {
+        institucion: "SENA",
+        curso: "Tecnologo en analisis y desarrollo de softwaare",
+        año: "2023",
+      },
+      {
+        institucion: "SENA",
+        curso: "Python",
+        año: "2023",
+      },{
+        institucion: "SENA",
+        curso: "React",
+        año: "2024",
+      },{
+        institucion: "SENA",
+        curso: "Base de datos",
+        año: "2024",
+      },{
+        institucion: "SENA",
+        curso: "Logica de programacion",
+        año: "2022",
+      },{
+        institucion: "SENA",
+        curso: "JavaScript",
+        año: "2025",
+      }
+    ]
+
+    return(
+      <div>
+        <h2>Educacion</h2>
+
+        {educacion.map((exp, index) =>(
+          <div key={index} style={{marginBottom: "15px"}}>
+            <h3>{exp.curso} - {exp.institucion} </h3>
+            <p><strong>año:</strong> {exp.año}</p>
+        
+          </div>
+
+        ))}
+
+      </div>
+    )
+}
+
+export default Educacion;
   
